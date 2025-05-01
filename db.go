7b6cc17d5a3bd1a7e4efc9ae45702fb1d9445a4b
caasmo <226161+caasmo@users.noreply.github.com>
2025-05-01 02:04:45 +0200

    chore: Remove unused db.go file

--- db.go
+++
@@ -1,9 +0,0 @@
-package acme // Or root package of your module
-
-// Writer defines the interface for storing certificate history records.
-// Types like Cert are available directly as they are in the same package.
-type Writer interface {
-	// AddCert adds a new certificate record to the database history.
-	AddCert(cert Cert) error // Use Cert directly
-	// Potentially add: GetLatestByIdentifier(identifier string) (*Cert, error)
-}
